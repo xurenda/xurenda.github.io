d50d0d3d6636e185aad33e0c29ca8cdfcdc51380
xurenda <xurenda@qq.com>
2023-11-21 20:55:44 +0800

    rm: content

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,14 +3,14 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "My-Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", ".git", ".gitignore", "README.md"],
     defaultDateType: "created",
     theme: {
       typography: {
